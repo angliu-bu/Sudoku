89e64ca74be27fc002497e8bd540f23dd501e01f
usef-kh <yousif.khaireddin@gmail.com>
2020-06-23 17:22:37 -0400

    Started creating GUI

--- GUI.py
+++ GUI.py
@@ -1,8 +1,8 @@
 import pygame
 
 pygame.init()
-size = w, h = 500,500
-color = (255,255,255)
+size = w, h = 500, 500
+color = (255, 255, 255)
 
 window = pygame.display.set_mode(size)
 window.fill(color)
@@ -14,10 +14,8 @@ while run:
     events = pygame.event.get()
     keys = pygame.key.get_pressed()
 
-
     if keys[pygame.K_ESCAPE]:
         run = False
     pygame.time.delay(100)
-
 
 pygame.quit()
