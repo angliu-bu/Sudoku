4f89e92930ef6c9e55201937d900fd62f76d7789
usef-kh <yousif.khaireddin@gmail.com>
2020-06-23 17:22:05 -0400

    Started creating GUi

---
+++ GUI.py
@@ -0,0 +1,23 @@
+import pygame
+
+pygame.init()
+size = w, h = 500,500
+color = (255,255,255)
+
+window = pygame.display.set_mode(size)
+window.fill(color)
+pygame.display.set_caption("Sudoku")
+pygame.display.update()
+
+run = True
+while run:
+    events = pygame.event.get()
+    keys = pygame.key.get_pressed()
+
+
+    if keys[pygame.K_ESCAPE]:
+        run = False
+    pygame.time.delay(100)
+
+
+pygame.quit()
